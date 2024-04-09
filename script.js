a5f4b4e214e9b205f8d634b100218533e312a974
Muhammed Abiodun <muhammedabiodun42@gmail.com>
2024-04-09 13:15:56 +0100

    commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const hamMenu = document.querySelector('.hamburger-menu');
+const offScreenMenu = document.querySelector('.off-screen-menu');
+const container = document.querySelector('.container')
+
+hamMenu.addEventListener('click', () =>{
+  hamMenu.classList.toggle('active')
+  offScreenMenu.classList.toggle('active')
+});
